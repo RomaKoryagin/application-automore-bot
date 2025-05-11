45be31ae74371249364d4274c8ba67bea32f0881
koryagin_rs <koryagin_rs@farpost.com>
2025-05-11 09:11:25 +0200

    create main file with registering structs

---
+++ cmd/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"alex.com/application-bot/internal/application/factories"
+	"alex.com/application-bot/internal/application/services"
+	"alex.com/application-bot/internal/application/strategies"
+	"alex.com/application-bot/internal/infrastructure/repositories"
+	"alex.com/application-bot/internal/infrastructure/sqlite"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+func main() {
+	executableFolderPath, _ := os.Getwd()
+
+	fmt.Println(executableFolderPath)
+
+	db := sqlite.Database{MainDirPath: executableFolderPath}
+
+	db.Init()
+
+	bot, err := tgbotapi.NewBotAPI("7696650759:AAGmVzXsbXus2fGxkr3Xr9Awy5l4XcyBTEg")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	/** repositories */
+	userRepository := repositories.NewUserRepository(db.Connection)
+	applicationRepository := repositories.NewApplicationRepository(db.Connection)
+	/** end repositories */
+
+	/** services */
+	userService := services.NewUserService(userRepository)
+	applicationService := services.NewApplicationService(applicationRepository)
+	/** end services */
+
+	/** factories */
+	factoryResolver := factories.NewFactoryResolver()
+	/** end factories */
+
+	/** strategies */
+	startCommandStrategy := strategies.NewStartCommandStrategy(userService, applicationService)
+	updateApplicationStrategy := strategies.NewUpdateApplicationStrategy(userService, applicationService, factoryResolver)
+	websiteComamandStrategy := strategies.NewWebsiteCommandStrategy()
+	aboutCommandStrategy := strategies.NewAboutCommandStrategy()
+	newApplicationStrategy := strategies.NewNewApplicationStrategy(userService, applicationService)
+	/** end strategies */
+
+	/** resolvers */
+	strategyResolver := strategies.NewStrategyResolver()
+
+	strategyResolver.AddStrategy(startCommandStrategy)
+	strategyResolver.AddStrategy(updateApplicationStrategy)
+	strategyResolver.AddStrategy(websiteComamandStrategy)
+	strategyResolver.AddStrategy(aboutCommandStrategy)
+	strategyResolver.AddStrategy(newApplicationStrategy)
+	/** end resolvers */
+
+	telegramMessageService := services.NewTelegramMessageService(bot, strategyResolver)
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message != nil {
+			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+			go telegramMessageService.SendReplyMessage(update.Message.Chat.ID, update.Message.Text)
+		}
+	}
+}
