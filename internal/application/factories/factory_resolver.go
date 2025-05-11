7a3d363e97737593df5d03a0e575d8757de9b93b
koryagin_rs <koryagin_rs@farpost.com>
2025-05-11 09:09:27 +0200

    create factory resolver struct

---
+++ internal/application/factories/factory_resolver.go
@@ -0,0 +1,46 @@
+package factories
+
+import (
+	"errors"
+
+	"alex.com/application-bot/internal/application/enums"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+type MessageFactory interface {
+	CreateMessage(chatId int64) *tgbotapi.MessageConfig
+}
+
+type FactoryResolver struct {
+}
+
+func (resolver FactoryResolver) Resolve(fType enums.StepType) (MessageFactory, error) {
+	switch fType {
+	case enums.Budget:
+		return &BudgetStepMessageFactory{}, nil
+	case enums.City:
+		return &CityStepMessageFactory{}, nil
+	case enums.Country:
+		return &CountryStepMessageFactory{}, nil
+	case enums.JapanWarning:
+		return &JapanWarningMessageFactory{}, nil
+	case enums.MarkOrConditions:
+		return &MarkOfConditionsStepMessageFactory{}, nil
+	case enums.PersonName:
+		return &PersonNameStepMessageFactory{}, nil
+	case enums.PersonPhone:
+		return &PersonPhoneStepMessageFactory{}, nil
+	case enums.Submit:
+		return &SubmitStepMessageFactory{}, nil
+	case enums.InvalidPhone:
+		return &InvalidPhoneMessageFactory{}, nil
+	case enums.SubmittedApplication:
+		return &SubmittedApplicationMessageFactory{}, nil
+	default:
+		return nil, errors.New("incorrent factory type found")
+	}
+}
+
+func NewFactoryResolver() *FactoryResolver {
+	return &FactoryResolver{}
+}
