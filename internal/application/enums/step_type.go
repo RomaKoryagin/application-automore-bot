86ec509dc0a30aa99c224d0f0cd990e8e9847168
koryagin_rs <koryagin_rs@farpost.com>
2025-05-11 09:06:22 +0200

    create enums for steps and strategies

---
+++ internal/application/enums/step_type.go
@@ -0,0 +1,16 @@
+package enums
+
+type StepType string
+
+const (
+	Budget               StepType = "Budget"
+	City                 StepType = "City"
+	Country              StepType = "Country"
+	JapanWarning         StepType = "JapanWarning"
+	MarkOrConditions     StepType = "MarkOrConditions"
+	PersonName           StepType = "PersonName"
+	PersonPhone          StepType = "PersonPhone"
+	Submit               StepType = "Submit"
+	InvalidPhone         StepType = "InvalidPhone"
+	SubmittedApplication StepType = "SubmittedApplication"
+)
---
+++ internal/application/enums/strategy_type.go
@@ -0,0 +1,11 @@
+package enums
+
+type StrategyType string
+
+const (
+	Start             StrategyType = "Start"
+	About             StrategyType = "About"
+	UpdateApplication StrategyType = "UpdateApplications"
+	WebsiteLink       StrategyType = "WebsiteLink"
+	NewApplication    StrategyType = "NewApplication"
+)
