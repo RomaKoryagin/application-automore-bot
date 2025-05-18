ae4d17ea09ceafc29f6618cb14cb8f083b9e5764
koryagin_rs <koryagin_rs@farpost.com>
2025-05-18 12:37:58 +0200

    update phone format message

--- internal/application/factories/person_phone_step_message_factory.go
+++ internal/application/factories/person_phone_step_message_factory.go
@@ -8,7 +8,7 @@ type PersonPhoneStepMessageFactory struct {
 }
 
 func (factory PersonPhoneStepMessageFactory) CreateMessage(chatId int64) *tgbotapi.MessageConfig {
-	msg := tgbotapi.NewMessage(chatId, "Пожалуйста, укажите Ваш номер телефона в формате 7ХХХХХХХХХ ⚠️Номер нужен для авторизации, проверка защиты от ботов, благодарим за понимание.")
+	msg := tgbotapi.NewMessage(chatId, "Пожалуйста, укажите Ваш номер телефона в формате 7ХХХХХХХХХХ ⚠️Номер нужен для авторизации, проверка защиты от ботов, благодарим за понимание.")
 
 	replyKeyboard := tgbotapi.NewRemoveKeyboard(true)
 	msg.ReplyMarkup = replyKeyboard
